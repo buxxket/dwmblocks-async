aec5d6060f7f148f1df5a6d0c822c4c058b816fa
Dusan <oalfodr@gmail.com>
2022-09-14 04:36:33 +0200

    First config

--- config.h
+++ config.h
@@ -1,16 +1,13 @@
 #define CMDLENGTH 45
-#define DELIMITER "  "
+#define DELIMITER " | "
 #define CLICKABLE_BLOCKS
+#define LEADING_DELIMITER
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+	BLOCK("cpubars",  2,   1),
+	BLOCK("memory",   2,   2),
+	BLOCK("battery",  5,   3),
+	BLOCK("internet", 5,   4),
+	BLOCK("volume",   0,   5),
+	BLOCK("clock",    60,  6),
 };
