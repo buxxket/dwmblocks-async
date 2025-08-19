539bad183189c45eda8ceeb29715809f2ee56ab4
buxxket <buxxket@noreply.codeberg.org>
2025-08-19 23:40:28 +1000

    Initial setup

---
+++ config.def.h
@@ -0,0 +1,17 @@
+#define CMDLENGTH 100
+#define DELIMITER " "
+#define CLICKABLE_BLOCKS
+#define LEADING_DELIMITER
+
+const Block blocks[] = {
+	BLOCK("sb-ha-phone-battery",	10, 1),
+	BLOCK("sb-device-status",		5,  2),
+	BLOCK("sb-todo",				1,  3),
+	BLOCK("sb-m30-sync",			2,  4),
+	BLOCK("sb-music",				1,  5),
+	BLOCK("sb-clock",				1,  6),
+	BLOCK("sb-volume",				5,  7),
+	BLOCK("sb-bluetooth",			5,	8),
+	BLOCK("sb-battery",				60,	9),
+	BLOCK("sb-internet",   			5,	10),
+};
